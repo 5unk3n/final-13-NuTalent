319a56051181fcd4ebeda8f8328e2ae048fc8e55
Seongeun Lee <tjsrms1227@gmail.com>
2023-10-05 12:36:36 +0900

    Refactor: app, index 포맷팅 및 필요 없는 로직 정리

--- src/App.js
+++ src/App.js
@@ -1,45 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import Toast from './components/Toast/Toast';
 import Alert from './components/common/Alert/Alert';
-import Router from './routes/Router';
-import { recoilData } from './recoil/atoms/dataState';
-import { instance, setupInterceptor } from './util/api/axiosInstance';
-import { useAlert, useBottomSheet, useToast } from './hooks/useModal';
 import BottomSheetModal from './components/common/BottomSheetModal/BottomSheetModal';
+import Toast from './components/Toast/Toast';
+import { useAlert, useBottomSheet, useToast } from './hooks/useModal';
 import queryClient from './libs/react-query';
+import Router from './routes/Router';
 
 function App() {
-  const [isReady, setIsReady] = useState(false);
-  const token = useRecoilValue(recoilData).token;
   const { toast } = useToast();
   const { alert } = useAlert();
   const { bottomSheet } = useBottomSheet();
 
-  useEffect(() => {
-    const interceptorId = setupInterceptor(token);
-    setIsReady(true);
-    return () => {
-      instance.interceptors.request.eject(interceptorId);
-    };
-  }, [token]);
-
   return (
-    isReady && (
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <Router />
-          <Toast {...toast} />
-          <Alert {...alert} />
-          <BottomSheetModal {...bottomSheet} />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </BrowserRouter>
-    )
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <Router />
+        <Toast {...toast} />
+        <Alert {...alert} />
+        <BottomSheetModal {...bottomSheet} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import ReactDOM from 'react-dom'; //구버전
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import { GlobalStyle } from './styles/GlobalStyle';
 import { Fragment } from 'react';
+import { createRoot } from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
+
+import App from './App';
 import theme from './styles/DefaultTheme';
+import { GlobalStyle } from './styles/GlobalStyle';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
