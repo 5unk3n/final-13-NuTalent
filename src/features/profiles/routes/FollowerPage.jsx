4f64c4f28dcec72afb6bc8678d43ac7923c47357
Seongeun Lee <tjsrms1227@gmail.com>
2023-10-08 22:45:37 +0900

    Refactor: FollowList 컴포넌트 내에서 데이터 받도록 수정

--- src/features/profiles/routes/FollowerPage.jsx
+++ src/features/profiles/routes/FollowerPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-import { useGetFollowerList } from '../api/getFollowerList';
 import FollowList from '../components/FollowList';
 
 import HeaderBar from '@/components/Elements/HeaderBar';
@@ -9,17 +8,13 @@ import HeaderBar from '@/components/Elements/HeaderBar';
 export default function FollowerPage() {
   const { accountname } = useParams();
 
-  const { data: followerList, isLoading } = useGetFollowerList(accountname);
-
-  if (isLoading) return;
-
   return (
     <>
       <HeaderBar>
         <HeaderBar.BackButton />
-        <HeaderBar.Title />
+        <HeaderBar.Title size="sm">Follower</HeaderBar.Title>
       </HeaderBar>
-      <FollowList userList={followerList} />
+      <FollowList accountname={accountname} mode="follower" />
     </>
   );
 }
