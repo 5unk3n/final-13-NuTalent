abe4443b9f7aa3dbded6306d271d4a7ef0aff2bf
Seongeun Lee <tjsrms1227@gmail.com>
2023-10-11 00:16:32 +0900

    Fix: 게시글, 상품 기능 생성, 수정 시 에러 토스트 추가

--- src/features/products/api/updateProduct.js
+++ src/features/products/api/updateProduct.js
@@ -1,6 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
+import { useToast } from '@/hooks/useModal';
 import instance from '@/libs/axios';
 
 const updateProduct = async (productId, product) => {
@@ -10,9 +11,12 @@ const updateProduct = async (productId, product) => {
 
 export const useUpdateProduct = (productId) => {
   const navigate = useNavigate();
+  const { openToast } = useToast();
 
   return useMutation({
     mutationFn: (product) => updateProduct(productId, product),
     onSuccess: () => navigate(-1),
+    onError: (error) =>
+      openToast({ message: error.response.data.message, status: 'error' }),
   });
 };
