fdded4e7bc4833c31897fca0bc2ec1ef90a8710a
Seongeun Lee <tjsrms1227@gmail.com>
2023-10-11 11:47:41 +0900

    Fix: 댓글 역순으로 나오는 문제 수정

--- src/features/comments/components/CommentList.jsx
+++ src/features/comments/components/CommentList.jsx
@@ -8,19 +8,18 @@ export default function CommentList() {
   const { id: postId } = useParams();
 
   const { data: commentList, isLoading } = useGetCommentList(postId);
+  console.log(commentList);
 
   if (isLoading) return;
 
   return (
     <ul>
-      {commentList.pages.map((page) => {
-        return page.map((comment) => {
-          return (
-            <li key={comment.id}>
-              <Comment comment={comment} postId={postId} />
-            </li>
-          );
-        });
+      {commentList.map((comment) => {
+        return (
+          <li key={comment.id}>
+            <Comment comment={comment} postId={postId} />
+          </li>
+        );
       })}
     </ul>
   );
